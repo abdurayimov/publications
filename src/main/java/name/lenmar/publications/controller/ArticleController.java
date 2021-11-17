0440785f0ee6d351abb30cfbd8c3064de2667b91
abdurayimov <abdurayimov@gmail.com>
2021-11-17 16:56:30 +0300

    ArticleController is created

--- src/main/java/name/lenmar/publications/controller/ArticleController.java
+++ src/main/java/name/lenmar/publications/controller/ArticleController.java
@@ -1,14 +1,33 @@
 package name.lenmar.publications.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("article")
 public class ArticleController {
+
     @GetMapping
-    public String list() {
-        return "index";
+    public String getAllArticles() {
+        return "All articles";
+    }
+
+    @GetMapping("{id}")
+    public String getArticleById(@PathVariable Long id) {
+        return "Article by id" + id;
+    }
+
+    @PostMapping
+    public String createNewArticle() {
+        return "New article is created";
+    }
+
+    @PutMapping("{id}")
+    public String editArticleById(@PathVariable Long id) {
+        return "Article by id is updated";
+    }
+
+    @DeleteMapping("{id}")
+    public String deleteArticleById(@PathVariable Long id) {
+        return "Article by id is deleted";
     }
 }
