3d420b2c5ae11bd561d5725f3e4a2516d0bf7698
abdurayimov <abdurayimov@gmail.com>
2021-11-17 16:31:26 +0300

    Initial Project

---
+++ src/main/java/name/lenmar/publications/controller/ArticleController.java
@@ -0,0 +1,14 @@
+package name.lenmar.publications.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("article")
+public class ArticleController {
+    @GetMapping
+    public String list() {
+        return "index";
+    }
+}
